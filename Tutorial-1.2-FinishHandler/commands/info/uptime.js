ddb6eb4e6fb342b0bc39d4bab4071b5b18e38627
Tomato#6966 <68145571+Tomato6966@users.noreply.github.com>
2020-12-17 13:25:48 +0100

    Update uptime.js

--- Tutorial-1.2-FinishHandler/commands/info/uptime.js
+++ Tutorial-1.2-FinishHandler/commands/info/uptime.js
@@ -1,19 +1,17 @@
 module.exports = {
     name: "uptime",
-	aliases: ["uptime"],
+    aliases: ["uptime"],
     category: "info",
-    description: "Returns latency and API ping",
-    run: async (client, message, args) => {
-    
-
-    function duration(ms) {
-        const sec = Math.floor((ms / 1000) % 60).toString()
-        const min = Math.floor((ms / (1000 * 60)) % 60).toString()
-        const hrs = Math.floor((ms / (1000 * 60 * 60)) % 60).toString()
-        const days = Math.floor((ms / (1000 * 60 * 60 * 24)) % 60).toString()
-        return `${days.padStart(1, '0')} Days, ${hrs.padStart(2, '0')} Hours, ${min.padStart(2, '0')} Minutes, ${sec.padStart(2, '0')} Seconds, `
-    }
-    
-    message.reply(`📈 **StripClub-Bot** is since ${duration(bot.uptime)} online.`);
+    description: "Returns the duration of how long the Bot is online",
+    usage: "[command], [aliases]",
+    run: async (client,message,args) => {
+        function duration(ms) {
+            const sec = Math.floor(ms / 1000 % 60).toString();
+            const min = Math.floor(ms / (60*1000) % 60).toString();
+            const hrs = Math.floor(ms / (60*60*1000) % 60).toString();
+            const days = Math.floor(ms / (24*60*60*1000) % 60).toString();
+            return `\`${days} Days\`, \`${hrs} Hours\`, \`${min} Minutes\`, \`${sec} Seconds\``
+        }
+        message.reply(`:white_check_mark: **${client.user.username}** is since ${duration(client.uptime)} online`);
     }
 }
