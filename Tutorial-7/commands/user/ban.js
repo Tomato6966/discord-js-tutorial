a07d98c99d1e22b9a2cc685117e3ef7be35b4275
Tomato#6966 <68145571+Tomato6966@users.noreply.github.com>
2021-01-02 16:48:16 +0100

    Update ban.js

--- Tutorial-7/commands/user/ban.js
+++ Tutorial-7/commands/user/ban.js
@@ -9,7 +9,7 @@ module.exports = {
 		if(!user) return message.reply("PLEASE PING A USER");
 		if(user.id === message.author.id) return message.reply("U cannot ban yourself");
 		let member = message.guild.members.cache.get(user.id);
-		if(!member.hasPermission("ADMINISTRATOR")) return message.reply("DU BIST KEIN ADMIN! nutze diesen cmd nicht!")
+		if(!message.member.hasPermission("ADMINISTRATOR")) return message.reply("DU BIST KEIN ADMIN! nutze diesen cmd nicht!")
 			if(!member.bannable)  return message.channel.send("Konnte den User nicht bannen!");
 		try{
 				member.ban();
